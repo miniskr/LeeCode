22fcb2b1676de6e6b01461c52e2603b36257ea7d
liuyt <numb327@gmail.com>
2021-03-28 14:13:57 +0800

    二叉树中序遍历

---
+++ Leecode.Solutions/94.InorderTraversal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Leecode.Solutions
+{
+    public partial class Solution
+    {
+        /*
+         * 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+         */
+        public static IList<int> InorderTraversal(TreeNode root)
+        {
+            var list = new List<int>();
+            Inorder(root, list);
+            return list;
+        }
+
+        private static void Inorder(TreeNode root, IList<int> res)
+        {
+            if (root is null)
+                return;
+
+            Inorder(root.left, res);
+            res.Add(root.val);
+            Inorder(root.right, res);
+        }
+    }
+}
---
+++ Leecode.Test/94.InorderTraversalTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leecode.Solutions;
+using Xunit;
+
+namespace Leecode.Test
+{
+    public class InorderTraversalTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var root1 = new TreeNode(1, null, new TreeNode(2, new TreeNode(3, null, null), null));
+            var res1 = new List<int>() { 1, 3, 2 };
+            var ret1 = Solution.InorderTraversal(root1);
+            Assert.Equal(res1, ret1);
+        }
+    }
+}

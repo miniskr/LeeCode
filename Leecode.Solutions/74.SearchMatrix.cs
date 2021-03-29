97198c924f189973c30f8f937dd1619d0c58cf25
liuyt <miniskr@qq.com>
2021-03-30 01:16:50 +0800

    搜索二位矩阵

---
+++ Leecode.Solutions/74.SearchMatrix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leecode.Solutions
+{
+    public partial class Solution
+    {
+        // 这里应该使用二分查找
+        public static bool SearchMatrix(int[][] matrix, int target)
+        {
+            if (target < matrix[0][0] || target > matrix[^1][^1])
+                return false;
+
+            var rows = matrix[0..^0];
+
+            foreach (var row in rows)
+            {
+                var columns = row[0..^0];
+                foreach (var cell in columns)
+                {
+                    if (cell == target)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Leecode.Test/74.SearchMatrixTest.cs
@@ -0,0 +1,22 @@
+﻿using Leecode.Solutions;
+using Xunit;
+
+namespace Leecode.Test
+{
+    public class SearchMatrixTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var matrix1 = new int[][] {
+                new int[] { 1, 3, 5, 7 },
+                new int[] { 10, 11, 16, 20 },
+                new int[] { 23, 30, 34, 60 }
+            };
+            var search1 = Solution.SearchMatrix(matrix1, 3);
+            Assert.True(search1);
+            var search2 = Solution.SearchMatrix(matrix1, 13);
+            Assert.False(search2);
+        }
+    }
+}

a5e6e9f58213c09e9adfaf5350f37c297b82dc2f
liuyt <miniskr@qq.com>
2021-04-06 12:59:21 +0800

    删除有序数组中的重复项

---
+++ Leecode.Solutions/26.RemoveDuplicates.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leecode.Solutions
+{
+    public partial class Solution
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            var i = 0;
+
+            for (var j = 1; j < nums.Length; j++)
+            {
+                if (nums[i] != nums[j])
+                {
+                    i++;
+                    nums[i] = nums[j];
+                }
+            }
+            return i + 1;
+        }
+    }
+}
---
+++ Leecode.Solutions/80.RemoveDuplicatesII.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leecode.Solutions
+{
+    public partial class Solution
+    {
+        public static int RemoveDuplicatesII(int[] nums)
+        {
+            var len = nums.Length;
+            if (len <= 2) return len;
+
+            var slow = 2;
+            var fast = 2;
+
+            while (fast < len)
+            {
+                if (nums[slow - 2] != nums[fast])
+                {
+                    nums[slow] = nums[fast];
+                    ++slow;
+                }
+                ++fast;
+            }
+
+            return slow;
+        }
+    }
+}

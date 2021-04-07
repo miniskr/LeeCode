f9e5c2d59772e523333b1a9ce20623b15daa5aa8
liuyt <miniskr@qq.com>
2021-04-07 14:23:16 +0800

    Search

---
+++ Leecode.Solutions/SearchInRotatedSortedArrayII.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leecode.Solutions
+{
+    public partial class Solution
+    {
+        public static bool Search(int[]nums,int target)
+        {
+            return nums.Contains(target);
+        }
+    }
+}

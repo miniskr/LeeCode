f438c2f1af7adfd425e86792f236b0be625b03e6
liuyt <liuyt@hc-vision.cn>
2021-05-13 14:26:10 +0800

    整数反转

---
+++ Leecode.Test/9.PalindromeNumberTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leecode.Solutions;
+using Xunit;
+
+namespace Leecode.Test
+{
+    public class PalindromeNumberTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var input1 = 121;
+            var input2= -121;
+            var input3 = 10;
+            var input4 = -101;
+
+            Assert.True(Solution.IsPalindrome(input1));
+            Assert.False(Solution.IsPalindrome(input2));
+            Assert.False(Solution.IsPalindrome(input3));
+            Assert.False(Solution.IsPalindrome(input4));
+            
+        }
+    }
+}

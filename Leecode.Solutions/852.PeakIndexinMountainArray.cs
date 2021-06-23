41350cf8b6a86bd0f546833a4f003d514fff1934
liuyt <miniskr@qq.com>
2021-06-23 09:22:07 +0800

    add peakindex

---
+++ Leecode.Solutions/852.PeakIndexinMountainArray.cs
@@ -0,0 +1,36 @@
+﻿namespace Leecode.Solutions
+{
+    public partial class Solution
+    {
+        public static int PeakIndexinMountainArray(int[] arr)
+        {
+            var arr1 = arr;
+            var index = 0;
+            while (arr1.Length > 1)
+            {
+                arr1 = BinaryCompare(arr1, out index);
+            }
+
+            return index;
+        }
+
+        private static int[] BinaryCompare(int[] arr, out int index)
+        {
+            var len = arr.Length;
+            var binLen = len / 2;
+            var left = arr[..binLen];
+            var right = arr[binLen..];
+
+            if (left[binLen - 1] > right[0])
+            {
+                index = binLen - 1;
+                return left;
+            }
+            else
+            {
+                index = binLen;
+                return right;
+            }
+        }
+    }
+}
---
+++ Leecode.Test/PeakIndexinMountainArrayTest.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace Leecode.Test
+{
+    public class PeakIndexinMountainArrayTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var arr1 = new int[] { 0, 1, 0 };
+            var ass1 = 1;
+            var arr2 = new int[] { 0, 2, 1, 0 };
+            var ass2 = 1;
+            var arr3 = new int[] { 0, 10, 5, 2 };
+            var ass3 = 1;
+            var arr4 = new int[] { 3, 4, 5, 1 };
+            var ass4 = 2;
+            var arr5 = new int[] { 24, 69, 100, 99, 79, 78, 67, 36, 26, 19 };
+            var ass5 = 2;
+
+            var res1 = Solutions.Solution.PeakIndexinMountainArray(arr1);
+            var res5 = Solutions.Solution.PeakIndexinMountainArray(arr5);
+            Assert.Equal(ass1, res1);
+            var res2 = Solutions.Solution.PeakIndexinMountainArray(arr2);
+            Assert.Equal(ass2, res2);
+            var res3 = Solutions.Solution.PeakIndexinMountainArray(arr3);
+            Assert.Equal(ass3, res3);
+            var res4 = Solutions.Solution.PeakIndexinMountainArray(arr4);
+            Assert.Equal(ass4, res4);
+            Assert.Equal(ass5, res5);
+        }
+    }
+}

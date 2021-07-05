8a7cd014565d6145d9997244471875a371910c92
miniskr <miniskr@qq.com>
2021-07-05 18:02:01 +0800

    add maxsubarray

---
+++ Leecode.Solutions/53.MaxSubArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Leecode.Solutions
+{
+    public partial class Solution
+    {
+        public static int MaxSubArray(int[] nums)
+        {
+            var pre = 0;
+            var maxAns = nums[0];
+            foreach (var num in nums)
+            {
+                pre = Math.Max(pre + num, num);
+                maxAns = Math.Max(pre, maxAns);
+            }
+            return maxAns;
+        }
+
+    }
+
+    public class Status
+    {
+        public int lSum, rSum, mSum, iSum;
+        public Status(int lSum, int rSum, int mSum, int iSum)
+        {
+            this.lSum = lSum;
+            this.rSum = rSum;
+            this.mSum = mSum;
+            this.iSum = iSum;
+        }
+    }
+}
---
+++ Leecode.Test/MaxSubArrayTest.cs
@@ -0,0 +1,19 @@
+﻿using Leecode.Solutions;
+using Xunit;
+
+namespace Leecode.Test
+{
+    public class MaxSubArrayTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var arr1 = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            Assert.Equal(6, Solution.MaxSubArray(arr1));
+            var arr2 = new int[] { 1 };
+            Assert.Equal(1, Solution.MaxSubArray(arr2));
+            var arr3 = new int[] { 5, 4, -1, 7, 8 };
+            Assert.Equal(23, Solution.MaxSubArray(arr3));
+        }
+    }
+}

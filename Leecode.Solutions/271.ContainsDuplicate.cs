78c00d4eb61c897803fe8136c9733086a86f5f6d
miniskr <miniskr@qq.com>
2021-07-05 10:35:50 +0800

    Contains Duplicate

---
+++ Leecode.Solutions/271.ContainsDuplicate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leecode.Solutions
+{
+    public partial class Solution
+    {
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            var hash = new HashSet<int>(nums);
+            return nums.Length != hash.Count;
+        }
+    }
+}
---
+++ Leecode.Test/271.ContainsDuplicateTest.cs
@@ -0,0 +1,21 @@
+﻿using Leecode.Solutions;
+using Xunit;
+
+namespace Leecode.Test
+{
+    public class ContainsDuplicateTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var array1 = new[] { 1, 2, 3, 1 };
+            Assert.True(Solution.ContainsDuplicate(array1));
+
+            var array2 = new[] { 1, 2, 3, 4 };
+            Assert.False(Solution.ContainsDuplicate(array2));
+
+            var array3 = new[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+            Assert.True(Solution.ContainsDuplicate(array3));
+        }
+    }
+}

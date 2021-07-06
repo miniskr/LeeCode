d9d61f5df1b455bcffc9cd116cd4f3da9c654a95
miniskr <miniskr@qq.com>
2021-07-06 10:32:11 +0800

    binarysearch

---
+++ Leecode.Solutions/704.BinarySearch.cs
@@ -0,0 +1,26 @@
+﻿namespace Leecode.Solutions
+{
+    public partial class Solution
+    {
+        /// <summary>
+        /// 二分查找
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int BinarySearch(int[] nums, int target)
+        {
+            int pivot;
+            var left = 0;
+            var right = nums.Length - 1;
+            while (left <= right)
+            {
+                pivot = left + (right - left) / 2;
+                if (nums[pivot] == target) return pivot;
+                if (target < nums[pivot]) right = pivot - 1;
+                else left = pivot + 1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Leecode.Test/BinarySearchTest.cs
@@ -0,0 +1,17 @@
+﻿using Leecode.Solutions;
+using Xunit;
+
+namespace Leecode.Test
+{
+    public class BinarySearchTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var arr1 = new int[] { -1, 0, 3, 5, 9, 12 };
+            Assert.Equal(4, Solution.BinarySearch(arr1, 9));
+            var arr2 = new int[] { -1, 0, 3, 5, 9, 12 };
+            Assert.Equal(-1, Solution.BinarySearch(arr2, 2));
+        }
+    }
+}
